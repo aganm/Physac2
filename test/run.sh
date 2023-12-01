d605a45d33d596081a6f6ff0f41ae25f54566be1
Michael Aganier <michaelaganier@gmail.com>
2023-12-01 15:05:26 -0500

    Adjust warnings

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm test.exe
-gcc -o test.exe main.c -s -static -lpthread -lm -std=gnu99 -Wall -Wextra
+gcc -o test.exe main.c -s -static -lpthread -lm -std=gnu99 -Wall -Wextra -Wshadow
 ./test.exe
