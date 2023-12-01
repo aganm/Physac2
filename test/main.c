fc55e0d02376db6e12b2f01eaa66c24f834ff151
Michael Aganier <michaelaganier@gmail.com>
2023-12-01 17:27:40 -0500

    Adjust test main

--- test/main.c
+++ test/main.c
@@ -10,9 +10,15 @@ int main(int argc, char **argv)
 	(void)argv;
 
 	InitPhysics();
+
+	PhysicsBody *body = CreatePhysicsBodyCircle((Vector2){ 0 }, 0.0f, 0.0f);
+	DestroyPhysicsBody(body);
+
 	for (int i = 0; i < 1000; ++i) {
 		RunPhysicsStep();
 	}
+
 	ClosePhysics();
+
 	return 0;
 }
