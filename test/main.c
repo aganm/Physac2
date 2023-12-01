ea97327488586e4f4f3c60ff66c2fa22c24da9de
Michael Aganier <michaelaganier@gmail.com>
2023-12-01 14:34:07 -0500

    Adjust test main

--- test/main.c
+++ test/main.c
@@ -1,10 +1,18 @@
 #define PHYSAC_STANDALONE
 #define PHYSAC_NO_THREADS
 #define PHYSAC_IMPLEMENTATION
+#define PHYSAC_DEBUG
 #include "../src/physac.h"
 
-int main() {
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
 	InitPhysics();
-	RunPhysicsStep();
+	for (int i = 0; i < 1000; ++i) {
+		RunPhysicsStep();
+	}
+	ClosePhysics();
 	return 0;
 }
